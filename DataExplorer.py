2a572979dfbdf6bb245ef7d623df1f57b35a5e42
leonbbrown <leon8223@outlook.com>
2024-05-06 18:26:47 +0100

    Update DataExplorer.py

--- DataExplorer.py
+++ DataExplorer.py
@@ -34,19 +34,21 @@ def check_payment():
    
 def check_sku():
     most_popular_sku = sku_column.mode().values
+    print('These are the most popular products:')
+    print(most_popular_sku)
     
-    print('the most popular product is:')
-    if most_popular_sku == 174832:
-        print('Jeans')
-    elif most_popular_sku == 126594:
-        print('T-shirt')
-    elif most_popular_sku == 198765:
-        print('Hoodie')
-    elif most_popular_sku == 142307:
-        print('Knitted Sweater')
-    elif most_popular_sku == 186905:
-        print('Socks')
-    else: #195472
-        print('Boxer Breifs')
+    #print('the most popular product is:')
+    #if most_popular_sku == '174832':
+        #print('Jeans')
+    #elif most_popular_sku == '126594':
+        #print('T-shirt')
+    #elif most_popular_sku == '198765':
+        #print('Hoodie')
+    #elif most_popular_sku == '142307':
+        #print('Knitted Sweater')
+    #elif most_popular_sku == '186905':
+        #print('Socks')
+    #else: #195472
+        #print('Boxer Breifs')
 
 check_sku()
